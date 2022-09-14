6d429133abb50d312fab830abc99aab819c64dd3
GianmariaApruzzese <113304048+GianmariaApruzzese@users.noreply.github.com>
2022-09-14 15:01:51 +0200

    Add files via upload

---
+++ first_code.py
@@ -0,0 +1,35 @@
+from re import I
+
+
+print("_________________________________________\n\n")
+print("hello world");
+#variabili --> tasca --> ospita dati
+#dichiariamo uan variabile 
+#in python i tipi non c'è bisogno di esplicitarli, python non è fortemente tipato, lo deduce dal contesto
+#pyhton non ha bisogno dei punti e virgola
+print("_________________________________________\n\n")
+
+a=5;
+b=13
+a=10+a;
+c=b+a
+print(a, "ciao\n")
+print(c);
+d=b/c
+print(d)
+i=0
+print("valore c prima ciclo for",c)
+for  i in range(10):
+    c=c+1
+#tornando a capo e mettendomi tutto a festra è uscito dal ciclo for da solo
+print(c)
+d=40
+j=0
+for j in range(d):
+    print(j)
+    
+
+c=c+1
+print("\n")
+print(c)
+print("\n");
